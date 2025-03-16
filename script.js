45ae864f544834ddbc8805201671d97076ec0d7e
Dylan <nalyd27@outlook.com>
2025-03-16 18:20:34 -0300

    Fixing bugs and Finishing script

--- script.js
+++ script.js
@@ -1,10 +1,58 @@
 document.querySelectorAll(".trash-icon").forEach((icon) => {
   icon.addEventListener("click", function (event) {
-    // Impede que o clique na lixeira seja propagado para a .box
     event.stopPropagation();
 
-    // Remove a caixa que contém a lixeira
-    const box = icon.closest(".box"); // Encontra a .box mais próxima da lixeira
-    box.remove(); // Remove a .box da página
+    const box = icon.closest(".box");
+    box.remove();
+
+    const alertBox = document.querySelector(".alert");
+    alertBox.style.display = "flex";
   });
 });
+
+const closeButton = document.querySelector(".alert button");
+
+closeButton.addEventListener("click", function () {
+  const alertBox = closeButton.closest(".alert");
+  alertBox.style.display = "none";
+});
+
+document
+  .getElementById("add-item-button")
+  .addEventListener("click", function () {
+    const inputValue = document.getElementById("item-form").value;
+
+    if (inputValue.trim() !== "") {
+      const newBox = document.createElement("label");
+      newBox.classList.add("box");
+
+      const radioInput = document.createElement("input");
+      radioInput.type = "radio";
+      radioInput.name = "opcao";
+      radioInput.value = inputValue;
+
+      const span = document.createElement("span");
+      span.textContent = inputValue;
+
+      const trashIcon = document.createElement("div");
+      trashIcon.classList.add("trash-icon");
+
+      newBox.appendChild(radioInput);
+      newBox.appendChild(span);
+      newBox.appendChild(trashIcon);
+
+      document.getElementById("box-list").appendChild(newBox);
+
+      document.getElementById("item-form").value = "";
+
+      trashIcon.addEventListener("click", function (event) {
+        event.stopPropagation();
+        newBox.remove();
+
+        const alertBox = document.querySelector(".alert");
+        alertBox.style.display = "flex";
+      });
+    } else {
+      alert("Por favor, insira um item válido!");
+    }
+  });
